7852bf9e34ccb2f003e439e416e9216fabf01382
AnneSophieFstn <annesophie.faustino@gmail.com>
2025-01-15 13:36:17 +0400

    Fix bug + made the home background with react native skia - Day 13

--- day12/App.tsx
+++ day12/App.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
-const App = () => {
-  const width = 256;
-  const height = 256;
-  const r = width * 0.33;
+import { StatusBar } from "expo-status-bar";
+import HomeBackground from "./src/components/HomeBackground";
+export default function App() {
   return (
-    <Canvas style={{ width, height }}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={width - r} cy={r} r={r} color="magenta" />
-        <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
-      </Group>
-    </Canvas>
+    <>
+      <HomeBackground />
+      <StatusBar style="light" />
+    </>
   );
-};
-export default App;
+}
